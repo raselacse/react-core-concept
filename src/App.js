933961612a685ef890c89c12acdbfb71d46487a1
Md. Rasel Hossain <rasel.as.cse@gmail.com>
2021-02-23 07:39:05 +0600

    how to access object property and set style

--- src/App.js
+++ src/App.js
@@ -1,22 +1,22 @@
 import logo from './logo.svg';
 import './App.css';
+import reactDom from 'react-dom';
 
 function App() {
+  const person = {
+    name: "Rasel",
+    age: 25
+  }
+  const style ={
+    color: 'black',
+    backgroundColor: 'white'
+  }
   return (
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <h3 style={style}>Name: {person.name}</h3>
+        <p style={{backgroundColor: 'cyan', color: 'black'}}>Age: {person.age}</p>
       </header>
     </div>
   );
