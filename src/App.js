c393602b1f8659ce0147aec78b32bc4585488d51
Md. Rasel Hossain <rasel.as.cse@gmail.com>
2021-02-23 23:47:59 +0600

    component state hook and set state method

--- src/App.js
+++ src/App.js
@@ -1,22 +1,9 @@
 import logo from './logo.svg';
 import './App.css';
 import reactDom from 'react-dom';
+import { useState } from 'react';
 
-function Person(props){
-  const personStyle ={
-    border: '1px solid yellow',
-    padding: '10px',
-    margin: '10px',
-    width: '400px'
-  }
-  return(
-    <div style={personStyle}>
-      <h3>{props.name}</h3>
-      <h4>{props.age}</h4>
-    </div>
-  );
-}
-
+const names = ['Shawon',"Farid",'Rafsan']
 const products = [{name: 'Photoshop', price: '$90.99'},
                 {name: 'Illustrator', price: '$80.99'},
                 {name: 'PDF Reader', price: '$70.99'}]
@@ -39,18 +26,43 @@ function Product(props){
   )
 }
 
+function Counter(){
+  const [count, setCount] = useState(10);
+  // const handleIncrease = () =>{
+  //   const newCount = count + 1;
+  //   setCount(newCount);
+  // } 
+  // const handleDecrease= () =>{
+  //   const newCount = count - 1;
+  //   setCount(newCount);
+  // } 
+  return(
+    <div>
+      <h1>Count: {count}</h1>
+      <button onClick={() => setCount(count+1)}>Increase</button>
+      <button onClick={() => setCount(count-1)}>Decrease</button>
+    </div>
+  )
+}
+
 // Web UI
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <Product product={products[0]}></Product>
-        <Product product={products[1]}></Product>
-        <Product product={products[2]}></Product>
-        <Person name="Md. Rasel Hossain" age="25"></Person>
-        <Person name="Md. Shawon Hossain" age="27"></Person>
-        <Person name="Md. Silencer Hossain" age="15"></Person>
+        <Counter></Counter>
+        <ul>
+          {
+            products.map(product => <li>{product.name}</li>)
+          }
+          {
+            names.map(name => <li>{name}</li>)
+          }
+        </ul>
+        {
+          products.map(product => <Product product={product}></Product>)
+        }
       </header>
     </div>
   );
