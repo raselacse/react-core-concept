f08e4d53536f43bd5e04d30486eb8ec8e275895f
Md. Rasel Hossain <rasel.as.cse@gmail.com>
2021-02-23 21:37:36 +0600

    pass object to component and access object

--- src/App.js
+++ src/App.js
@@ -17,12 +17,37 @@ function Person(props){
   );
 }
 
+const products = [{name: 'Photoshop', price: '$90.99'},
+                {name: 'Illustrator', price: '$80.99'},
+                {name: 'PDF Reader', price: '$70.99'}]
+
+function Product(props){
+  const ProductStyle = {
+    height: '300px',
+    width: '400px',
+    border: '1px solid gray',
+    borderRadius: '5px',
+    backgrouondColor: 'lightwhite'
+  }
+  const {name, price} = props.product;
+  return(
+    <div style={ProductStyle}>
+      <h3>{name}</h3>
+      <h1>{price}</h1>
+      <button>Buy Now</button>
+    </div>
+  )
+}
+
 // Web UI
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
+        <Product product={products[0]}></Product>
+        <Product product={products[1]}></Product>
+        <Product product={products[2]}></Product>
         <Person name="Md. Rasel Hossain" age="25"></Person>
         <Person name="Md. Shawon Hossain" age="27"></Person>
         <Person name="Md. Silencer Hossain" age="15"></Person>
