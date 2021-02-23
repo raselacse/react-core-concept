2881cef33c1f70770222b807a3c3ad290373ce82
Md. Rasel Hossain <rasel.as.cse@gmail.com>
2021-02-23 19:25:28 +0600

    pass dynamic data to components using props

--- src/App.js
+++ src/App.js
@@ -2,29 +2,32 @@ import logo from './logo.svg';
 import './App.css';
 import reactDom from 'react-dom';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <Person></Person>
-        <Person></Person>
-        <Person></Person>
-      </header>
-    </div>
-  );
-}
-function Person(){
+function Person(props){
   const personStyle ={
     border: '1px solid yellow',
     padding: '10px',
-    margin: '10px'
+    margin: '10px',
+    width: '400px'
   }
   return(
     <div style={personStyle}>
-      <h3>Name: Md. Rasel Hossain</h3>
-      <h4>Age: 25</h4>
+      <h3>{props.name}</h3>
+      <h4>{props.age}</h4>
     </div>
   );
 }
 
+// Web UI
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <Person name="Md. Rasel Hossain" age="25"></Person>
+        <Person name="Md. Shawon Hossain" age="27"></Person>
+        <Person name="Md. Silencer Hossain" age="15"></Person>
+      </header>
+    </div>
+  );
+}
 export default App;
